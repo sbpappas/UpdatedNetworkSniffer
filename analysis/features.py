ff4acc2a17af602841a64265c2c01c3472150638
Samuel Pappas <spappas@trinity.edu>
2026-02-01 08:42:23 -0600

    add packet aggregation

---
+++ analysis/features.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+def aggregate_by_ip(packets):
+    # takes a bunch of packets and divides them by source IP
+    
+    features = defaultdict(lambda: {
+        "packets_sent": 0,
+        "packets_received": 0,
+        "bytes_sent": 0,
+        "bytes_received": 0,
+        "unique_dst_ports": set(),
+        "protocols_used": set(),
+    })
+
+    for pkt in packets:
+        src = pkt["src_ip"]
+        dst = pkt["dst_ip"]
+        length = pkt["length"]
+        proto = pkt["protocol"]
+        dst_port = pkt["dst_port"]
+
+        # source IP behavior
+        features[src]["packets_sent"] += 1
+        features[src]["bytes_sent"] += length
+        if dst_port:
+            features[src]["unique_dst_ports"].add(dst_port)
+        if proto:
+            features[src]["protocols_used"].add(proto)
+
+        # destination IP behavior
+        features[dst]["packets_received"] += 1
+        features[dst]["bytes_received"] += length
+        if proto:
+            features[dst]["protocols_used"].add(proto)
+
+    return features
