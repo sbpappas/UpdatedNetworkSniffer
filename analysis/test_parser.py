d5e4dd712e553092a3cc3542c440543016fa4f09
Samuel Pappas <spappas@trinity.edu>
2026-02-02 22:11:23 -0600

    starting the detection for suspicious behavior

--- analysis/test_parser.py
+++ analysis/test_parser.py
@@ -2,7 +2,7 @@ from parser import parse_packets
 
 count = 0
 
-for pkt in parse_packets("data/traffic.pcap"): ..
+for pkt in parse_packets("data/traffic.pcap"): # print first 100 packets using the parser and yield!
     print(pkt)
     count += 1
     if count == 100:
