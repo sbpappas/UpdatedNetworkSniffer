1878924d6fac69d795fe093ae6c42ece22ca7515
Samuel Pappas <spappas@trinity.edu>
2026-02-05 22:17:37 -0600

    added documentation

--- reporting/run_dashboard.py
+++ reporting/run_dashboard.py
@@ -8,3 +8,7 @@ features = aggregate_by_ip(packets)
 plot_packets_sent_per_host(features)
 
 # run with python3 -m reporting.run_dashboard
+# that allows us to run the file as a module so that the imports work correctly. 
+# If we just run python3 reporting/run_dashboard.py, the imports will fail because
+# the current directory won't be in the module search path. By using -m, we ensure
+# that the current directory is treated as a package and the imports work as expected.
