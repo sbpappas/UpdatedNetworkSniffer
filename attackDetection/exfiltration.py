259dbb6fe8ac864edfc7ba983d426facce7a5f42
Samuel Pappas <spappas@trinity.edu>
2026-02-15 20:42:47 -0600

    data exfiltration if we detect large amounts of data

---
+++ attackDetection/exfiltration.py
@@ -0,0 +1,23 @@
+def detect_data_exfiltration(features):
+    # detect hosts sending at ton of outbound traffic
+
+    alerts = []
+
+    if not features:
+        return alerts
+
+    total_outbound = [data["bytes_sent"] for data in features.values()]
+    avg_outbound = sum(total_outbound) / len(total_outbound)
+
+    for ip, data in features.items():
+
+        if data["bytes_sent"] > avg_outbound * 3:
+            alerts.append({
+                "type": "DATA_EXFILTRATION",
+                "source_ip": ip,
+                "bytes_sent": data["bytes_sent"],
+                "average_bytes_sent": int(avg_outbound),
+                "severity": "HIGH"
+            })
+
+    return alerts
